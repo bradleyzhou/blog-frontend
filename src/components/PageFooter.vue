11a0d7f9d0ffe09f82a9515b12d2e732c7ab3b33
Bradley Zhou <bangyuzhou@gmail.com>
2017-05-27 00:59:31 +0800

    open link in another tab/window

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -4,17 +4,17 @@
 
 <template lang="html">
   <footer>
-    <p class="footer-text footer-col">Copyright © 2017 Bradley Zhou. All Rights Reserved.</p>
+    <p class="footer-text footer-col">© 2017 Bradley Zhou. All Rights Reserved.</p>
     <ul class="footer-col">
       <li>
-        <a href="https://github.com/bradleyzhou">
+        <a target="_blank" href="https://github.com/bradleyzhou">
           <svg aria-hidden="true" class="octicon octicon-mark-github"  version="1.1" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
           </svg>
         </a>
       </li>
       <li>
-        <a href="https://twitter.com/zhoubradley">
+        <a target="_blank" href="https://twitter.com/zhoubradley">
           <svg version="1.1" class="twitter-icon" viewBox="0 0 400 400">
           <path  d="M400,200c0,110.5-89.5,200-200,200S0,310.5,0,200S89.5,0,200,0S400,89.5,400,200z M163.4,305.5
           	c88.7,0,137.2-73.5,137.2-137.2c0-2.1,0-4.2-0.1-6.2c9.4-6.8,17.6-15.3,24.1-25c-8.6,3.8-17.9,6.4-27.7,7.6
@@ -38,11 +38,8 @@ export default {
 <style lang="css">
 footer {
   height: 35px;
-  padding-left: 40px;
-  padding-right: 40px;
-  padding-top: 20px;
+  padding: 20px 40px 20px 40px;
   margin-top: 40px;
-  margin-bottom: 20px;
   border-top: 1px solid #D0DAF1;
   display: flex;
   align-items: center;
