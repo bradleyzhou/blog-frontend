c4f53cc81ac11eedf37b32773c98abdf5647f10a
Bradley Zhou <bangyuzhou@gmail.com>
2017-05-26 17:18:12 +0800

    init component/page for single blog posts

---
+++ src/components/Post.vue
@@ -0,0 +1,16 @@
+<docs>
+  The post content page, shows a single blog-post.
+</docs>
+
+<template lang="html">
+
+</template>
+
+<script>
+export default {
+  name: 'post'
+}
+</script>
+
+<style lang="css">
+</style>
